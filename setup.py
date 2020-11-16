c25532b9aabcf28df83af7998ce198b91829570f
seryogin17 <seregin-gena@mail.ru>
2020-11-16 16:30:48 +0300

    deleted hyphen

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pre-commit-callgraph',
-      packages=find_packages(include=['pre-commit-callgraph']),
+      packages=['precommitcallgraph'],
       include_package_data=True,
       scripts=['scripts/pre-commit-callgraph'],
       zip_safe=False)
