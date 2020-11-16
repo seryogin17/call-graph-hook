2b59aaa772e9fdd4966e7ebb10b6032222a9e37a
seryogin17 <seregin-gena@mail.ru>
2020-11-16 17:23:29 +0300

    changed setup

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from setuptools import setup, find_packages
 
 setup(name='pre-commit-callgraph',
-      setup_requires=['setuptools_scm'],
-      use_scm_version=True,
       packages=['precommitcallgraph'],
       include_package_data=True,
       scripts=['scripts/pre-commit-callgraph'],
