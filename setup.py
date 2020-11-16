29da2dcfa93bc206c09073d4cfd256446485d093
seryogin17 <seregin-gena@mail.ru>
2020-11-16 16:14:34 +0300

    include

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pre-commit-callgraph',
-      packages=find_packages(),
+      packages=find_packages(include=['pre-commit-callgraph']),
       include_package_data=True,
       scripts=['scripts/pre-commit-callgraph'],
       zip_safe=False)
