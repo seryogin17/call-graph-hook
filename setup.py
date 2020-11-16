1c90a0fea47e077f995f90dcd6b31cb0da8e9abd
seryogin17 <seregin-gena@mail.ru>
2020-11-16 17:58:24 +0300

    removed scripts dir

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(name='pre-commit-callgraph',
       packages=['precommit'],
       include_package_data=True,
-      scripts=['scripts/pre-commit-callgraph.py'],
+      scripts=['precommit/pre-commit-callgraph.py'],
       zip_safe=False)
