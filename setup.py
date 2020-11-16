dacbe9265cbd669a0f8dc5e36e7d2d0ae9334469
seryogin17 <seregin-gena@mail.ru>
2020-11-16 16:44:42 +0300

    req

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(name='pre-commit-callgraph',
+      setup_requires=['setuptools_scm'],
+      use_scm_version=True,
       packages=['precommitcallgraph'],
       include_package_data=True,
       scripts=['scripts/pre-commit-callgraph'],
