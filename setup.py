e81b2b74d29d7d4e5ffed8b8b69ac3e66f81bdcd
seryogin17 <seregin-gena@mail.ru>
2020-11-16 16:51:12 +0300

    reqs

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+mfrom setuptools import setup, find_packages
 
 setup(name='pre-commit-callgraph',
       setup_requires=['setuptools_scm'],
@@ -6,4 +6,7 @@ setup(name='pre-commit-callgraph',
       packages=['precommitcallgraph'],
       include_package_data=True,
       scripts=['scripts/pre-commit-callgraph'],
+      install_requires=[
+        'pandas',
+        'logging'],
       zip_safe=False)
