ffa3cd86c4d45bad0bc9e9400b5f9fd379faa142
Gennady Seryogin <46023311+seryogin17@users.noreply.github.com>
2020-11-17 19:21:34 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(name='callgraph',
       packages=['callgraph'],
       include_package_data=True,
-      scripts=['callgraph/callgraph.py'],
+      scripts=['callgraph/callgraph_wrapper.py'],
       zip_safe=False)
